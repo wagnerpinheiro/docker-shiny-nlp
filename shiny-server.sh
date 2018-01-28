8d9265f7442a8226a5be6c4510c79843fa3c6b9e
wagner.pinheiro <wagner@wagnerpinheiro.com.br>
2018-01-28 14:36:08 -0200

    create a data dir with write permission

--- shiny-server.sh
+++ shiny-server.sh
@@ -3,5 +3,7 @@
 # Make sure the directory for individual app logs exists
 mkdir -p /var/log/shiny-server
 chown shiny.shiny /var/log/shiny-server
+mkdir /srv/shiny-server/data
+chown shiny:shiny /srv/shiny-server/data
 
 exec shiny-server 2>&1
